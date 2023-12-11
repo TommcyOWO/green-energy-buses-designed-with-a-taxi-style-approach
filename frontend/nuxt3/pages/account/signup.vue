c01e02abe3c557381a1bd0fbb5796a1b85a0d55a
TommcyOWO <tangminghong.tom@gmail.com>
2023-12-11 20:43:41 +0800

    update

--- frontend/nuxt3/pages/account/signup.vue
+++ frontend/nuxt3/pages/account/signup.vue
@@ -5,31 +5,32 @@
           <span class="block pt:10px">
             <h2>使用者名稱</h2>
             <h4>User Name</h4>
-            <input class="acinpu" type="text">
+            <input v-model="username" class="acinpu" type="text">
           </span>
           <span class="block my:10px">
             <h2>電子郵件</h2>
             <h4>E-Mail</h4>
-            <input class="acinpu" type="email">
+            <input v-model="email" class="acinpu" type="email">
           </span>
           <span class="block my:10px">
             <h2>密碼</h2>
             <h4>Password</h4>
-            <input class="acinpu" type="password">
+            <input v-model="password" class="acinpu" type="password">
           </span>
         </nav>
         <section class=" mx:40px">
           <button @click="moveTo('account-login')" class="btn cursor:pointer">登入</button>
           <button @click="moveTo('account-password_forgot')" class="btn ml:30px cursor:pointer">忘記密碼?</button>
-          <button class="btn w:200px cursor:pointer mt:10px">註冊</button>
+          <button @click="sign_up()" class="btn w:200px cursor:pointer mt:10px">註冊</button>
         </section>
       </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import "axios";
+import axios from "axios";
 import Cookies from 'cookies-ts';
+import conf from "@/assets/conf";
 
 const username = ref("")
 const password = ref("")
@@ -38,6 +39,21 @@ const email = ref("")
 const router = useRouter();
 const moveTo = (path: string) => router.push({ name: path });
 const cookies = new Cookies();
+
+
+const sign_up = async () => {
+  const data = {
+    "username":username.value,
+    "password":password.value,
+    "email":email.value
+  }
+  try {
+    const response = await axios.post(conf.urls+"sign_up",data)
+    moveTo("account-login")
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 onMounted(() => {
   const authKey = cookies.get("auth_key")
