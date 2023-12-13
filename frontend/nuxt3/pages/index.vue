2f4b044d00c70f4666fd53141a59b5c913ef33e3
TommcyOWO <tangminghong.tom@gmail.com>
2023-12-13 10:06:28 +0800

    updae

--- frontend/nuxt3/pages/index.vue
+++ frontend/nuxt3/pages/index.vue
@@ -1,6 +1,21 @@
 <template>
-  <div>
-  </div>
+  <div class="abs top:100px left:50% translate(-50%,50%)">
+    <nav>
+      <span class="block">
+        <h3>出發地</h3>
+        <h4>Origin</h4>
+        <input class="acinpu" type="text">
+      </span>
+      <span class="block my:20px">
+        <h3>目的地</h3>
+        <h4>Destination</h4>
+        <input class="acinpu" type="text">
+      </span>
+    </nav>
+    <section>
+      <button class="btn blcok">叫車</button>
+    </section>
+</div>
 </template>
 
 <script setup lang="ts">
